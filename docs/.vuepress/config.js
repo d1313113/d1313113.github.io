3f9f5bc7ff305b5bcb7103c6a2f75f810f0fe3ac
cumelmell <cumelmell@foxmail.com>
2019-08-13 17:57:16 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.export = {
+  title: 'Hello githubPage',
+  description: 'jjjj',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@alias': 'path/tp/soma/dir'
+      }
+    }
+  }
+};
