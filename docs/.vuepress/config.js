cc8759c62c86e9295a977266e6bafa3c0610a8a9
cumelmell <cumelmell@foxmail.com>
2019-09-19 01:11:25 +0800

    Add(algorithm/readme)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,46 @@
-module.export = {
+module.exports = {
   title: 'Hello githubPage',
   description: 'jjjj',
+  repo: 'https://github.com/d1313113/d1313113.github.io',
+  // dest: './dist',
   configureWebpack: {
     resolve: {
       alias: {
         '@alias': 'path/tp/soma/dir'
       }
     }
+  },
+  plugins: [
+    'vuepress-plugin-cat'
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: 'github',
+        link: 'https://github.com/d1313113/d1313113.github.io'
+      }
+    ],
+    serviceWorker: {
+      updatePopup: {
+        message: '内容有更新',
+        buttonText: '刷新'
+      }
+    },
+    sidebar: [
+      {
+        title: 'algorithm',
+        collapsable: true,
+        children: [
+          ['/algorithm/', '算法']
+        ]
+      },
+      {
+        title: 'Redis',
+        collapsable: true,
+        children: [
+          ['/redis/', 'Redis']
+        ]
+      }
+    ]
   }
 };
